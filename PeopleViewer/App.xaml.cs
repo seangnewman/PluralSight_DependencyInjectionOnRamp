510f023634b0ad3cf6bc797f675cba65da671cc3
newmanuevers <seangnewman@msn.com>
2018-12-16 00:23:10 -0700

    Removed tight-coupling, added repositories and cache

---
+++ PeopleViewer/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using PeopleViewer.Presentation;
+using PersonRepository.CachingDecorator;
+using PersonRepository.CSV;
+using PersonRepository.Service;
+using PersonRepository.SQL;
+using System.Windows;
+
+namespace PeopleViewer
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ComposeObjects();
+            Application.Current.MainWindow.Title = "Loose Coupling - People Viewer";
+            Application.Current.MainWindow.Show();
+        }
+
+        private static void ComposeObjects()
+        {
+
+            //var repository = new ServiceRepository();
+            // var repository = new CSVRepository();
+            //var repository = new SQLRepository();
+            var wrappedRepository = new ServiceRepository();
+            var repository = new CachingRepository(wrappedRepository);
+            var viewModel = new PeopleViewerViewModel(repository);
+            Application.Current.MainWindow = new PeopleViewerWindow(viewModel);
+        }
+    }
+}
