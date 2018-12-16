b7a35c92b29d8ad4fd03c02a165febffab3acd8f
newmanuevers <seangnewman@msn.com>
2018-12-16 14:22:09 -0700

    Course Completion

---
+++ PeopleViewer.Ninject/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Ninject;
+using PersonRepository.Interface;
+using PersonRepository.Service;
+using System.Windows;
+
+namespace PeopleViewer
+{
+    public partial class App : Application
+    {
+
+        IKernel Container;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ConfigureContainer();
+            ComposeObjects();
+            Application.Current.MainWindow.Show();
+        }
+
+        private void ConfigureContainer()
+        {
+            Container = new StandardKernel();
+
+            Container.Bind<IPersonRepository>()
+                                       . To<ServiceRepository>()
+                                       .InSingletonScope();
+        }
+
+        private void ComposeObjects()
+        {
+            Application.Current.MainWindow = Container.Get<PeopleViewerWindow>();
+            Application.Current.MainWindow.Title = "DI with Ninject - People Viewer";
+        }
+    }
+}
